5ca69b53d1b6cf1eeda09005874cc3176a3b5100
David Pease <david.f.pease@gmail.com>
2020-11-30 12:37:59 -0800

    Initial commit of file structure, db setup, and Express setup

---
+++ app.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const app = express();
+const port = process.env.PORT || 5000;
+const db = require('./config/keys').mongoURI;
+const mongoose = require('mongoose');
+mongoose
+  .connect(db, { useNewUrlParser: true })
+  .then(() => console.log("Connected to MongoDB successfully"))
+  .catch(err => console.log(err));
+
+const users = require("./routes/api/users");
+const bodyParser = require('body-parser');
+
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+app.listen(port, () => console.log(`Server is running on port ${port}`));
+app.get("/", (req, res) => res.send("Hello World"));
+app.use("/api/users", users);
