ad3691db64f66deaa7d0753d6a720f285e3d0054
David Pease <david.f.pease@gmail.com>
2020-11-30 13:51:47 -0800

    Add backend user registration and login

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require("express");
 const app = express();
 const port = process.env.PORT || 5000;
 const db = require('./config/keys').mongoURI;
+const passport = require('passport');
 const mongoose = require('mongoose');
 mongoose
   .connect(db, { useNewUrlParser: true })
@@ -16,5 +17,7 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 app.listen(port, () => console.log(`Server is running on port ${port}`));
-app.get("/", (req, res) => res.send("Hello World"));
+app.use(passport.initialize());
+require('./config/passport')(passport);
+
 app.use("/api/users", users);
