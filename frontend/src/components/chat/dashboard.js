e46a060f89aac5899f2ddc3ae3ac81f86d4f9336
David Pease <david.f.pease@gmail.com>
2020-12-02 16:10:00 -0800

    add chat dashboard

---
+++ frontend/src/components/chat/dashboard.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import ChatBox from './chat_box_container';
+
+class DashBoard extends React.Component{
+
+   render(){
+      return(
+
+         <div>
+            <ChatBox/>    
+
+         </div>
+
+
+      )
+   }
+}
+
+
+export default DashBoard;
