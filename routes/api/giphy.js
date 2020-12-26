36e83d7af3b87b3b8f96580ab8d4a404f56feeac
Harry Gao <gaohaoyuandd@gmail.com>
2020-12-26 14:27:37 -0500

    deleted Pillars folder

--- routes/api/giphy.js
+++ routes/api/giphy.js
@@ -4,7 +4,7 @@ const request = require('request');
 // const mongoose = require('mongoose');
 const passport = require('passport');
 router.get('/:keyword', (req, res) => {
-   ;
+   
    request({
        uri: `http://api.giphy.com/v1/gifs/search?&api_key=nhYcIGL37mj4LYPqC1Dk1eDt4A074SeD&q=${req.params.keyword}&limit=5`
 
