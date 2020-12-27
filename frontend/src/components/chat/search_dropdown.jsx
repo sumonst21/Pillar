a8d752607739133b715d128397ab7abee83db56b
harry <gaohaoyuandd@gmail.com>
2020-12-27 01:27:26 -0500

    boyer moore added

--- frontend/src/components/chat/search_dropdown.jsx
+++ frontend/src/components/chat/search_dropdown.jsx
@@ -5,20 +5,77 @@ class SearchBarDropdown extends React.Component {
     constructor(props) {
         super(props)
 
+        this.customizedJoin = this.customizedJoin.bind(this);
         this.objectifiedMessages = this.objectifiedMessages.bind(this);
-    }
+        this.boyer_moore = this.boyer_moore.bind(this);
+    };
     
+    customizedJoin(arr, joiner, replacer=''){//customized join method to filter out the gifs and replace them with an optional input
+        let res = '';
+        for(let i=0; i<arr.length; i++){
+            if(arr[i].slice(0,8) == 'https://' && arr[i].slice(arr[i].length-4, arr[i].length) == '.gif'){
+                arr.splice(i, 1, replacer);
+            } 
+            res += `${arr[i]}${joiner}`; 
+            
+        };
+        return res.slice(0, -1);
+    };
     
     objectifiedMessages(obj){//get an object with roomJoined's title as the key and each room's messages as values
         const messageArr = [];
         Object.entries(obj).forEach(room => {
             const newObj = {};
-            // debugger
             newObj[room[1].title]=room[1].messages.map(m=>(m.message));
             messageArr.push(newObj);
         });
-        return messageArr.join('Þ');
+        // messageArr.join('Þ');
+        return messageArr;
     };
+
+    boyer_moore(str, sub){//using customized Boyer-Moore searching algorithm for faster lookup time for each input
+        let skip;
+        let res = [];
+        let stopper = 0;
+        let counter = 0;
+        let bad_char = new Array(265).fill(-1);
+
+        for(let t=0; t<sub.length; t++){//constructing bad character table for each chatacter in the substring at its corresponding place in 256 ASCII characters
+            const index = sub[t].charCodeAt();
+            bad_char[index] = t;
+        };
+
+        for(let i=0; i<=str.length-sub.length; i+=skip){//compare each character from substring to string, if mismatch, then shift to the next matching character; if no matching character found, shift the entire length of the substring
+            skip=0;
+            for(let j = sub.length-1; j>=0; j--){
+                //conditional logic for str[i+j] is ascII to deal with emojis and gifs 
+                //if(str[i+j].charCodeAt() > 265)
+                if(sub[j] != str[i+j]){
+                    const asciiIndex = j-bad_char[str[i+j].charCodeAt()]
+                    skip = 1 > asciiIndex ? 1 : asciiIndex;
+                    break;
+                }
+            };
+
+            if(skip == 0){
+                let pos_obj={};
+                for(let x = stopper; x < i; x++){//stopper and counter are tracking variables to track how many 'Þ' has passed, which later will be used to index corresponding substrings for each chatroom
+                    if(str[x] === 'Þ'){
+                        counter += 1;
+                    };
+                };
+                stopper = i;
+                pos_obj[i]=counter;
+                res.push(pos_obj);
+                skip++;
+            }
+        };
+        return res; //result will look like [ { '2': 0 }, { '7': 2 }, { '13': 3 } ], where keys are starting index positions for matching substrings and values are how many 'Þ' has passed
+    };
+
+    roomAssigner(){//adding logic to determine which room it belongs to
+
+    }
 
 
     render() {
