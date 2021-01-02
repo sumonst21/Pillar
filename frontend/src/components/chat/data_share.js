c29323907b4c63efce34c58e2b94a3d3b8bd4ffa
harry <gaohaoyuandd@gmail.com>
2021-01-01 23:20:54 -0500

    pre push

--- frontend/src/components/chat/data_share.js
+++ frontend/src/components/chat/data_share.js
@@ -1,21 +1,28 @@
-class SharedData {
-    constructor(){
-        this.openOrClose = [false];
-        this.infoFromSearchbar = (bool) => {
-            this.openOrClose.pop();
-            this.openOrClose.push(bool);
-            console.log(this.openOrClose)
-        }; 
-    };
+// class SharedData {
+//     constructor(){
+//         this.openOrClose = [false];
+//         this.infoFromSearchbar = (bool) => {
+//             this.openOrClose.pop();
+//             this.openOrClose.push(bool);
+//             console.log(this.openOrClose)
+//         }; 
+//     };
 
 
-    infoToChatbox(){
+//     infoToChatbox(){
 
-        return this.openOrClose
-    };
+//         return this.openOrClose
+//     };
 
+// }
+
+// const instance = new SharedData();
+
+// export default instance;
+import ChatBox from './chat_box_container';
+import SearchBarDropdown from './search_dropdown';
+import React, {useState} from 'react';
+
+function BridgeReducer(state, action) {
+    //when clicked in searchbar, it dispatch the room the message belongs to the state
 }
-
-const instance = new SharedData();
-
-export default instance;
