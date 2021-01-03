d2c9ebee78660d7049566ff401829a4cabb5e4e2
harry <gaohaoyuandd@gmail.com>
2021-01-03 13:30:14 -0500

    chatroom responsive from searchdrop down

--- frontend/src/components/chat/data_share.js
+++ frontend/src/components/chat/data_share.js
@@ -4,6 +4,5 @@ const subject = new Subject();
 
 export const switches = {
     sendOpen: room => subject.next(room ),
-    clear: () => subject.next(),
     receiveOpen: () => subject.asObservable()
 };
