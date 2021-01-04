16ebe07b4f483305c1daf1691db880aced6c7477
David Pease <david.f.pease@gmail.com>
2021-01-04 14:02:06 -0800

    Add create new room text validator and error messaging

--- frontend/src/actions/session_actions.js
+++ frontend/src/actions/session_actions.js
@@ -43,17 +43,6 @@ export const signup = user => dispatch => (
         dispatch(receiveErrors(err.response.data))
     ))
 );
-    // const { token } = res.data;
-    // localStorage.setItem("jwtToken", token);
-    // APIUtil.setAuthToken(token);
-    // const decoded = jwt_decode(token);
-    // dispatch(receiveCurrentUser(decoded));
-//     dispatch(receiveUserSignIn())
-//   )
-//   .catch(
-//     err => { 
-//       return dispatch(receiveErrors(err.response.data))})
-//   };
 
 // Upon login, set the session token and dispatch the current user. Dispatch errors on failure.
 export const login = user => dispatch => (
