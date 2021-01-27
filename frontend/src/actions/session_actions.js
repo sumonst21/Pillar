7edfcb6f474175ea6274c8f5a50af7d12d19f6c7
David Pease <david.f.pease@gmail.com>
2021-01-27 11:57:42 -0800

    Log in user upon successful signup

--- frontend/src/actions/session_actions.js
+++ frontend/src/actions/session_actions.js
@@ -41,8 +41,11 @@ export const logoutUser = () => {
 
 export const signup = user => dispatch => (
     APIUtil.signup(user).then(res => {
-        dispatch(receiveUserSignIn())
-        return {errors: {}};
+        const { token } = res.data;
+        localStorage.setItem('jwtToken', token);
+        APIUtil.setAuthToken(token);
+        const decoded = jwt_decode(token);
+        dispatch(receiveCurrentUser(decoded))
     }).catch(err => (
         dispatch(receiveErrors(err.response.data))
     ))
